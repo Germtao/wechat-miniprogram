0f09dd2462918d946c73f41b60b77a2682a74d9f
tTao <>
2021-04-01 22:35:45 +0800

    text 组件

---
+++ weapp-demo02/pages/component/component-pages/wx-text/wx-text.js
@@ -0,0 +1,27 @@
+// wx-text.js
+var initData = 'this is first line\nthis is second line'
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    text: initData
+  },
+
+  extraLine: [],
+  add: function (e) {
+    this.extraLine.push('other line')
+    this.setData({
+      text: initData + '\n' + this.extraLine.join('\n')
+    })
+  },
+  remove: function (e) {
+    if (this.extraLine.length > 0) {
+      this.extraLine.pop()
+      this.setData({
+        text: initData + '\n' + this.extraLine.join('\n')
+      })
+    }
+  }
+})
