15af33ea54cd44f4198c058a7407ba71066e1979
QDSG <piftao1127@163.com>
2021-04-01 16:19:12 +0800

    小程序组件首页显示

--- weapp-demo02/pages/component/index.js
+++ weapp-demo02/pages/component/index.js
@@ -2,3 +2,14 @@ var pageData = {},
     type = [
       'view', 'content', 'form', 'interact', 'nav', 'media', 'map', 'canvas'
     ]
+
+pageData.widgetsToggle = function(e) {
+  var id = e.currentTarget.id, data = {};
+  for (var index = 0; index < type.length; ++index) {
+      data[type[index] + 'Show'] = false;
+  }
+  data[id + 'Show'] = !this.data[id + 'Show'];
+  this.setData(data);
+}
+
+Page(pageData)
