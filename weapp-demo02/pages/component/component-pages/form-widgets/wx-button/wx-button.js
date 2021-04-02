591746f665dfc341cd338dfead3096dd89b2ccfa
tTao <>
2021-04-02 21:20:24 +0800

    button 组件

---
+++ weapp-demo02/pages/component/component-pages/form-widgets/wx-button/wx-button.js
@@ -0,0 +1,40 @@
+// pages/component/component-pages/form-widgets/wx-button/wx-button.js
+var types = ['default', 'primary', 'warn']
+var pageObject = {
+  data: {
+    defaultSize: 'default',
+    primarySize: 'default',
+    warnSize: 'default',
+    disabled: false,
+    plain: false,
+    loading: false
+  },
+  setDisabled: function (e) {
+    this.setData({
+      disabled: !this.data.disabled
+    })
+  },
+  setPlain: function (e) {
+    this.setData({
+      plain: !this.data.plain
+    })
+  },
+  setLoading: function (e) {
+    this.setData({
+      loading: !this.data.loading
+    })
+  }
+}
+
+for (let i = 0; i < types.length; i++) {
+  (function (type) {
+    pageObject[type] = function (e) {
+      var key = type + 'Size'
+      var changedData = {}
+      changedData[key] = this.data[key] === 'default' ? 'mini' : 'default'
+      this.setData(changedData) 
+    }
+  })(types[i])
+}
+
+Page(pageObject)
